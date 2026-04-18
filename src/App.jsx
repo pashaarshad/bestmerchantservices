a704aa613540025fedec99d3a01fb3535e491955
pashaarshad <arshadpashaintern@gmail.com>
2026-04-18 14:49:53 +0530

    it started the work

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Solutions from './components/Solutions'
+import Contactless from './components/Contactless'
+import Terminals from './components/Terminals'
+import CTA from './components/CTA'
+import Benefits from './components/Benefits'
+import PriceMatch from './components/PriceMatch'
+import Testimonials from './components/Testimonials'
+import Gallery from './components/Gallery'
+import POS from './components/POS'
+import Blog from './components/Blog'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <Solutions />
+      <Contactless />
+      <Terminals />
+      <CTA />
+      <Benefits />
+      <PriceMatch />
+      <Testimonials />
+      <Gallery />
+      <POS />
+      <Blog />
+      <Footer />
+    </>
+  )
+}
+
+export default App
